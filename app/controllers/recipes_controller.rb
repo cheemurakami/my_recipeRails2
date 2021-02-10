0cda71dfe3fa30eaa8bb6f20b32096ae76d8ab6b
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-09 21:00:42 -0800

    Added edit and update action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,8 +18,21 @@ class RecipesController < ApplicationController
     else
       render 'new'
     end
-
   end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+  if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+
 
   private
     def recipe_params
