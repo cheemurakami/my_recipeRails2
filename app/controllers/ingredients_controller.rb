6886da2380dc8ff10a2790067138478a51056f80
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-10 11:31:02 -0800

    add form for ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,10 +1,17 @@
 class IngredientsController < ApplicationController
   def new
-
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = Ingredient.new(recipe_id: params[:recipe_id])
   end
   
   def create
-
+    recipe = Recipe.find(params[:recipe_id])
+    @ingredient = recipe.ingredient.create(ingredient_params)
+    if @ingredient.save
+      redirect_to recipe_path
+    else
+      render 'new'
+    end
   end
 
   private 
