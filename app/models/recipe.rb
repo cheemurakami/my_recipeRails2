78755548d48d34c8623389ccea3bf603e7b02f0d
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-12 13:14:01 -0800

    add relationships between recipe and category

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :ingredients
   has_many :steps
+  has_and_belongs_to_many :categories
 
   def self.search(search)
     Recipe.where("title ILIKE ? or description ILIKE ?", "%#{search}%", "%#{search}%")
