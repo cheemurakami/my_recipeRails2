892c11a2bd532c77fc1df0e06f9cdd4f7be306b5
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-11 16:20:20 -0800

    added recipe_id in form and added create method with params

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -5,6 +5,17 @@ class StepsController < ApplicationController
   end
 
   def create
-
+    @recipe = Recipe.find(params[:recipe_id])
+    @step = Step.new(step_params)
+    if @step.save
+      redirect_to recipe_path(@recipe), notice: "Step is successfully added!"
+    else
+      render 'new'
+    end
   end
+
+  private
+    def step_params
+      params.require(:step).permit(:index, :description, :recipe_id)
+    end
 end
