4f33627fba4eb63e45839b2e47763d0e94f79aa7
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-12 10:40:08 -0800

    fix search method to get data from 2 columns

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,6 @@ class Recipe < ApplicationRecord
   has_many :steps
 
   def self.search(search)
-    Recipe.where("title ILIKE ?", "%#{search}%")
+    Recipe.where("title ILIKE ? or description ILIKE ?", "%#{search}%", "%#{search}%")
   end
 end
