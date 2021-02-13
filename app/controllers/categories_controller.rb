24241c403938a879b0f46182cc5b25deda90ebcf
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-13 11:45:17 -0800

    add form and create controller for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,22 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.includes(:recipes)
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: "Category is successfully created!"
+    else
+      render "new"
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
